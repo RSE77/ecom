0fca83d9a1e1fa70055ee978c95e7775b48a49de
RSE77 <rysboards@yahoo.com>
2021-03-06 21:54:11 -0500

    added redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
 ReactDOM.render(
+  <Provider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
