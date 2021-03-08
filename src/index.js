1a54d95872d2c5d1c6124d27282418aeda4d5c9a
RSE77 <rysboards@yahoo.com>
2021-03-07 22:12:54 -0500

    set up userReducer and rootReducer. Replaced state in Header component.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import store from './redux/store.js';
+
 
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
+  
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
